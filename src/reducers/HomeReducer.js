dfe9d7091c2d18d104803870009dbd8c4858548f
Carlita <carla.severe@gmail.com>
2017-07-11 15:22:48 -0400

    first commit

---
+++ src/reducers/HomeReducer.js
@@ -0,0 +1,13 @@
+//remember that a reducer is a function that returns a piece of state
+
+export default function(){
+	return [
+		'Chad',
+		'Shane',
+		'Ian',
+		'Porscha',
+		'Carla',
+		'Yingrong'
+
+	]
+}
